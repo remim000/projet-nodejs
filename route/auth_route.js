e21d22a3b1e4e2fbe41bb2faf7896a33b503558b
Thomas <tribeiro@auchan.fr>
2023-03-24 15:45:47 +0100

    ThomasR : Ajout du fichier route pour l'authentification

--- route/auth_route.js
+++ route/auth_route.js
@@ -1,1 +1,12 @@
 //route pour les authentifications
+
+import express from 'express';
+import * as authController from '../controller/auth.controller.js';
+
+const router = express.Router();
+
+router.post('/login', authController.login);
+
+router.post('/signin', authController.signin);
+
+export default router;
