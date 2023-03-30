c50072d0a8c861d30067f2bbbe5c532f81d2d43e
Thomas BERTHIER <thomas.berthier@kpf-si.com>
2023-03-30 18:05:34 +0200

    ThomasB : Ajout récupération des posts, récupération d'un post et suppresion d'un post

--- route/post_route.js
+++ route/post_route.js
@@ -6,6 +6,9 @@ const postController = require('../controller/post_controller.js');
 const router = express.Router();
 
 router.post('/create', postController.create);
+router.delete('/:_id', postController.delete);
+router.get('/', postController.getAll)
+router.get('/:_id', postController.getOne)
 
 
 module.exports = router;
