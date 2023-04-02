88aa0a39d44f5cda5d242dfb81acfbd201123a2d
Thomas <tribeiro@auchan.fr>
2023-04-02 16:47:26 +0200

    Thomas.R : Modification du delete dans post_route

--- route/post_route.js
+++ route/post_route.js
@@ -9,10 +9,10 @@ const router = express.Router();
 
 router.post('/', auth, multer, postController.create);
 router.put('/', auth, multer, postController.update);
-router.delete('/:_id', auth,  postController.delete);
+router.delete('/', auth,  postController.delete);
 router.get('/', auth,  postController.getAll);
 router.get('/getOne/:_id', auth, postController.getOne);
-router.get('/:_id', auth, postController.getOneAndCommentaire);
+router.get('/:_id', auth, postController.getOneAndCommentaire);  
 router.post('/:_id/commentaire', auth, postController.createCommentaire);
 
 
