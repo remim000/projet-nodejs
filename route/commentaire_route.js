835c1a8d96c0f16a712473a83f3e739987fa487e
Thomas BERTHIER <thomas.berthier@kpf-si.com>
2023-04-02 11:42:54 +0200

    ThomasB : Changement route comme demandé par client, et chagement code pour le rendre fonctionnel

--- route/commentaire_route.js
+++ route/commentaire_route.js
@@ -6,7 +6,7 @@ const auth = require("./../middleware/auth");
 
 const router = express.Router();
 
-router.post('/create', auth, commentaireController.create);
+router.post('/:idPost', auth, commentaireController.create);
 router.delete('/:_id', auth,  commentaireController.delete);
 router.get('/', auth,  commentaireController.getAll)
 router.get('/:_id', auth,  commentaireController.getOne)
